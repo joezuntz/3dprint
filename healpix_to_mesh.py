7ce2e83a1fd95e7e56fbbaf6f2c5e89789b78212
Joe Zuntz <joezuntz@googlemail.com>
2019-07-26 15:04:05 +0100

    add hemisphere argument

--- healpix_to_mesh.py
+++ healpix_to_mesh.py
@@ -1,7 +1,8 @@
 import healpy
 import stl
+import numpy as np
 
-def healpix_to_mesh(healpix_map, vmin=None, vmax=None, radius_variation=0.05):
+def healpix_to_mesh(healpix_map, vmin=None, vmax=None, radius_variation=0.05, hemisphere='both'):
     """Make an STL mesh from a healpix map.
 
     """
@@ -18,17 +19,33 @@ def healpix_to_mesh(healpix_map, vmin=None, vmax=None, radius_variation=0.05):
     scale_min = 1 - radius_variation
     scale_max = 1 + radius_variation
 
+    pix = np.arange(npix, dtype=int)
+
     # get the full list of pixel corners
-    pix_corners = healpy.boundaries(nside, np.arange(npix))
+    pix_corners = healpy.boundaries(nside, pix)
+
+
+    if hemisphere == 'north' or 'hemisphere' == 'south':
+        _, _, z = healpy.pix2vec(nside, np.arange(npix))
+        if hemisphere == 'north':
+            w = z >= 0
+        elif hemisphere == 'south':
+            w = z < 0
+        pix = pix[w]
+        npix = len(pix)
+    elif hemisphere != 'both':
+        raise ValueError("hemisphere parameter must be 'north', 'south', or 'both'")
+
+
 
     # Scale the pixel corners according to the surrounding pixel values,
     # to make this a 3D thing
-    for i in range(npix):
+    for i in pix:
         # This is inefficient - vertices are shared between pixels, so
         # we could avoid calculating this scale more than once for each pixel
         # This is probably four times slower than it has to be.
         for j in range(4):
-            xyz = all_pix_corners[i,:,j]
+            xyz = pix_corners[i,:,j]
             # Use healpy to interpolate the neighbouring pixels to this
             # one to a single scalar value
             theta, phi = healpy.vec2ang(xyz)
@@ -45,7 +62,7 @@ def healpix_to_mesh(healpix_map, vmin=None, vmax=None, radius_variation=0.05):
 
     # Divide each pixel into two triangles and assign the vertices
     # of those triangles to the mesh vectors
-    for i in range(npix):
+    for i in pix:
         f = 2*i
         mesh.vectors[f,0] = pix_corners[i,:,0]
         mesh.vectors[f,1] = pix_corners[i,:,1]
@@ -61,7 +78,7 @@ def healpix_to_mesh(healpix_map, vmin=None, vmax=None, radius_variation=0.05):
 def example():
     T = healpy.read_map('wmap_ilc_9yr_v5.fits')
     T = healpy.ud_grade(T, nside_out=32)
-    M = healpix_to_mesh(T)
+    M = healpix_to_mesh(T, hemisphere='north')
     M.save('wmap_ilc_9yr_v5.stl')
 
 
